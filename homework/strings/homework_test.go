2fb663d538d85ee894462363f9d681b32281099a
nikita.a.zaytsev <nikita.a.zaytsev@onefactor.com>
2025-08-04 20:05:40 +0300

    COW buffer realization

--- homework/strings/homework_test.go
+++ homework/strings/homework_test.go
@@ -11,27 +11,57 @@ import (
 type COWBuffer struct {
 	data []byte
 	refs *int
-	// need to implement
 }
 
 func NewCOWBuffer(data []byte) COWBuffer {
-	return COWBuffer{} // need to implement
+	refs := 1
+	return COWBuffer{
+		data: data,
+		refs: &refs,
+	}
 }
 
 func (b *COWBuffer) Clone() COWBuffer {
-	return COWBuffer{} // need to implement
+	*b.refs++
+	return COWBuffer{
+		data: b.data,
+		refs: b.refs,
+	}
 }
 
 func (b *COWBuffer) Close() {
-	// need to implement
+	*b.refs--
+	if *b.refs == 0 {
+		b.data = nil
+	}
 }
 
 func (b *COWBuffer) Update(index int, value byte) bool {
-	return false // need to implement
+	if index < 0 || index >= len(b.data) {
+		return false
+	}
+
+	if *b.refs > 1 {
+		dataCopy := make([]byte, len(b.data))
+		copy(dataCopy, b.data)
+
+		*b.refs--
+
+		refsCopy := 1
+		b.data = dataCopy
+		b.refs = &refsCopy
+	}
+
+	b.data[index] = value
+	return true
 }
 
 func (b *COWBuffer) String() string {
-	return "" // need to implement
+	if len(b.data) == 0 {
+		return ""
+	}
+
+	return unsafe.String(unsafe.SliceData(b.data), len(b.data))
 }
 
 func TestCOWBuffer(t *testing.T) {
